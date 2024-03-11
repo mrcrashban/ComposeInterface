77259ec669a3f0a58c1aeec2308128877c529279
mrcrashban <metelevkirill4@gmail.com>
2024-03-11 14:22:12 +0300

    Added topBar

--- app/src/main/java/com/example/composeinterface/MainActivity.kt
+++ app/src/main/java/com/example/composeinterface/MainActivity.kt
@@ -1,15 +1,30 @@
 package com.example.composeinterface
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.rounded.List
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.composeinterface.ui.theme.ComposeInterfaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +32,46 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeInterfaceTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+               MainScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun GreetingPreview() {
-    ComposeInterfaceTheme {
-        Greeting("Android")
+fun MainScreen() {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(colors = topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.primary),
+                title = {
+                    Text(
+                    text = "Cashfy",
+                    fontSize = 35.sp,
+                    fontStyle = FontStyle.Italic,
+                    fontFamily = FontFamily.Serif)
+                        },
+                navigationIcon = {
+                    IconButton(onClick = { }) {
+                      Icon(imageVector = Icons.Filled.Menu,
+                      contentDescription = "Menu")
+                    }
+                },
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(imageVector = Icons.Filled.Settings,
+                            contentDescription = "Settings")
+                    }
+                }
+                )
+
+        }
+    ) {
+
     }
 }
